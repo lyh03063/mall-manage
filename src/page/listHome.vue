ac76914cfc0af1800cf8b1d59ecdbb44446f9820
CDC <1137156244@qq.com>
2019-06-10 16:59:24 +0800

    cdc修改

--- src/page/listHome.vue
+++ src/page/listHome.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <div class="TAC">
       <space height="80"></space>
-      <h1 class="FS24">欢迎使用管理后台</h1>
+      <h1 class="FS24">欢迎使用管理后台cdc修改</h1>
       <div class="time">上次登录时间：2019-5-18</div>
     </div>
 
