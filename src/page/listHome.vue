96220b67f3906dd09758c2f16e0ca957ca8f618c
戴国欣 <601030982@qq.com>
2019-06-10 17:03:27 +0800

    戴国欣修改

--- src/page/listHome.vue
+++ src/page/listHome.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <div class="TAC">
       <space height="80"></space>
-      <h1 class="FS24">欢迎使用管理后台  添加</h1>
+      <h1 class="FS24">欢迎使用管理后台1111</h1>
       <div class="time">上次登录时间：2019-5-18</div>
     </div>
 
