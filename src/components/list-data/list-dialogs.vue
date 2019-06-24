5d67d923ebac83165d2d256c5bb7db97dc4addc6
lyh <113381161@qq.com>
2019-06-24 15:11:26 +0800

    lyh

--- src/components/list-data/list-dialogs.vue
+++ src/components/list-data/list-dialogs.vue
@@ -15,7 +15,6 @@
             <!-- <slot :name="'slot_detail_item_'+item.prop" v-if="item.slot" ></slot> -->
 
             <slot :name="item.slot" :row="row" v-if="item.slot"></slot>
-
             <!--如果该字段带有formatter,使用formatter返回的代码输出-->
             <div v-else-if="item.formatter" v-html="item.formatter(row)"></div>
             <!--否则如果该字段带type是html，使用html原文输出-->
